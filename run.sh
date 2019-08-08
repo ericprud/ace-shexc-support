c33b9da7ebdc5dfa795b8e6be276e82fe5eea5f2
Eric Prud'hommeaux <eric+github@w3.org>
2019-08-08 11:13:27 +0200

    ~ move junk to ericprud/ace-santa-mode-docs/

--- run.sh
+++
@@ -1,1 +0,0 @@
-node ./Makefile.dryice.js -nc demo && (cd lib/ace/mode/_test && node highlight_rules_test.js -gen)
--- test.sh
+++
@@ -1,3 +0,0 @@
-POSITIVE=../shexTest/schemas/\*.shex NEGATIVE=../shexTest/negativeSyntax/\*.shex node lib/ace/mode/shexc_test.js
-node lib/ace/mode/shexc_worker_test.js
-node lib/ace/test/all.js
